1d4d910116c68763117d9ff17d29b441aad69212
jotsubiszi <szynszylxz@gmail.com>
2024-12-17 13:01:55 +0000

    huh

--- sniffer/main.c
+++ sniffer/main.c
@@ -2,13 +2,14 @@
 #include "uarting.h"
 #include <avr/common.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 
 sniffin MISO;
 sniffin MOSI;
 
-ISR(INT0_vect) { sniffMiso(&MOSI); }
+ISR(INT0_vect) { printf("world!"); }
 
-ISR(INT1_vect) { printf("%c\r\n", MOSI.INTChar); }
+ISR(INT1_vect) { printf("hello"); }
 
 // The interrupt execution response for all the enabled AVR® interrupts is four
 // clock cycles minimum
@@ -23,11 +24,11 @@ int main(void) {
   // is set by the RETI instruction to enable subsequent interrupts.
   // The I-bit can also be set and cleared by the application with the
   // SEI and CLI instructions, as described in the instruction set reference.
-  EICRA = 0b00001011;
+  EICRA = 0b00001111;
   // INT0 on rising
   // INT1 on falling
   EIMSK = 0b00000011;
-
+  printf("%i, %i", EICRA, EIMSK);
   while (1 == 1) {
   }
   return 0;
