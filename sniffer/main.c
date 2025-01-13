f2e2e0880e741eec7a6b775076e113cdf9923551
jotsubiszi <szynszylxz@gmail.com>
2025-01-13 21:36:14 +0100

    ITWORKSOUAHSDFUOHAS

--- sniffer/main.c
+++ sniffer/main.c
@@ -1,35 +1,50 @@
-#include "spiSniffing.h"
+#include "pinConf.h"
 #include "uarting.h"
 #include <avr/common.h>
 #include <avr/interrupt.h>
-#include <stdio.h>
+#include <stdint.h>
 
-sniffin MISO;
-sniffin MOSI;
+uint8_t flag;
 
-ISR(INT0_vect) { printf("world!"); }
-
-ISR(INT1_vect) { printf("hello"); }
+ISR(INT1_vect) { flag = 1; }
 
 // The interrupt execution response for all the enabled AVR® interrupts is four
 // clock cycles minimum
 
 int main(void) {
-
   pinsInit();
   USART0_init();
-  pinsInit();
   sei();
   // The I-bit is cleared by hardware after an interrupt has occurred, and
   // is set by the RETI instruction to enable subsequent interrupts.
   // The I-bit can also be set and cleared by the application with the
   // SEI and CLI instructions, as described in the instruction set reference.
-  EICRA = 0b00001111;
+  EICRA = 0b00001000;
   // INT0 on rising
   // INT1 on falling
-  EIMSK = 0b00000011;
-  printf("%i, %i", EICRA, EIMSK);
-  while (1 == 1) {
+  EIMSK = 0b00000010;
+  uint8_t counter = 0;
+  uint8_t shiftMiso = 0;
+  uint8_t shiftMosi = 0;
+
+  printf("%i\r\n", shiftMiso);
+  while (1) {
+    if (flag > 0) {
+      shiftMiso = (shiftMiso << 1) + readPinMISO();
+      shiftMosi = (shiftMosi << 1) + readPinMOSI();
+      printf("bit rec!\r\n");
+      counter++;
+      flag = 0;
+    }
+
+    if (counter == 8) {
+      printf("MISO: %i\r\n", shiftMiso);
+      printf("MOSI: %i\r\n", shiftMosi);
+      counter = 0;
+      flag = 0;
+      shiftMiso = 0;
+      shiftMosi = 0;
+    }
   }
   return 0;
 }
--- sniffer/pinConf.c
+++ sniffer/pinConf.c
@@ -5,15 +5,15 @@ void pinsInit() {
   DDRD &= ~(1 << PD3);
   // PD2 = INT0
   // PD3 = INT1
-  //PORTD |= (1 << PD2);
-  //PORTD |= (1 << PD3);
+  PORTD &= ~(1 << PD2);
+  PORTD &= ~(1 << PD3);
 
   DDRD &= ~(1 << PD4);
   DDRD &= ~(1 << PD5);
   // PD4 = MISO
   // PD5 = MOSI
-  PORTD |= (1 << PD4);
-  //PORTD |= (1 << PD5);
+  PORTD &= ~(1 << PD4);
+  PORTD &= ~(1 << PD5);
 }
 
 uint8_t readPinMISO() { return (PIND & (1 << PD4)) == 0 ? 0 : 1; }
